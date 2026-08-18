18b403d47e80f5cd111ec52f141a0cdb3ddd3c32
yashika548 <goyalyashika032@gmail.com>
2026-08-19 02:57:11 +0530

    Fix Groq model

--- server/src/services/aiServices.js
+++ server/src/services/aiServices.js
@@ -13,7 +13,7 @@ const generateResponse = async (prompt) => {
           Authorization: `Bearer ${process.env.GROQ_API_KEY}`,
         },
         body: JSON.stringify({
-          model: "llama-3.3-70b-versatile",
+         model: "llama-3.1-8b-instant",
           messages: [
             {
               role: "user",
